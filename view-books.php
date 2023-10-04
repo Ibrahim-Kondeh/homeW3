76d1ff60dc905d6fb8e30923cc07571de7e889f1
Ibrahim-Kondeh <122960252+Ibrahim-Kondeh@users.noreply.github.com>
2023-10-04 10:46:05 -0500

    Update view-books.php

--- view-books.php
+++ view-books.php
@@ -1,1 +1,31 @@
+<h1>Books</h1> 
+<div class="table-responsive">
+  <table class="table">
+    <thead>
+      <tr>
+ <th>ID</th>
+    <th>Title</th>
+      <th>Author</th>
+      <th>ISBN</th>
+      </tr>
+     
+    </thead>
+    <tbody>
+    <?php
+     while ($books = $book->fetch_assoc()) { 
+       ?>
+       <tr>
+       <td><?php echo $books['book_id']; ?></td>
+       <td><?php echo $books['title']; ?></td>
+        <td><?php echo $books['author']; ?></td>
+         <td><?php echo $books['isbn']; ?></td>
+       </tr>
+       <?php
+     }
 
+   ?> 
+
+      
+    </tbody>
+  </table>
+</div>
