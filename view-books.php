5bf3ba932f747c1f6f2ec2be9dade0921c12db8b
Ibrahim-Kondeh <122960252+Ibrahim-Kondeh@users.noreply.github.com>
2023-10-04 11:01:26 -0500

    Update view-books.php

--- view-books.php
+++ view-books.php
@@ -12,7 +12,7 @@
     </thead>
     <tbody>
     <?php
-     while ($book = $books->fetch_assoc()) { 
+     while ($book = $booksinstroe->fetch_assoc()) { 
        ?>
        <tr>
        <td><?php echo $book['book_id']; ?></td>
