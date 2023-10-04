b4942857fd67cbc6faec7e80fc7c7b9035517be9
Ibrahim-Kondeh <122960252+Ibrahim-Kondeh@users.noreply.github.com>
2023-10-04 10:38:45 -0500

    Update model-books.php

--- model-books.php
+++ model-books.php
@@ -1,1 +1,16 @@
+<?php
+function selectbook() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT book_id, title, author, isbn FROM `book`");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
 
+?>
