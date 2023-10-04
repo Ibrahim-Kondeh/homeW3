1fc7f007844735b52f9e3ae5768aa6a05e71cc02
Ibrahim-Kondeh <122960252+Ibrahim-Kondeh@users.noreply.github.com>
2023-10-04 12:59:10 -0500

    Update view-librarymember.php

--- view-librarymember.php
+++ view-librarymember.php
@@ -21,7 +21,7 @@
        <td><?php echo $librarymember['member_name']; ?></td>
         <td><?php echo $librarymember['address']; ?></td>
          <td><?php echo $librarymember['email']; ?></td>
-         <td><a href="borrowed_books.php?id=<?php echo $librarymember['member_id']; ?> ">Borrowed Books</a></td>
+       
         
        </tr>
        <?php
