89abab0f53e57fb23d8282e6742540bd6cebf3ee
Ibrahim-Kondeh <122960252+Ibrahim-Kondeh@users.noreply.github.com>
2023-10-04 23:26:27 -0500

    Update books-borrowed-by-member.php

--- books-borrowed-by-member.php
+++ books-borrowed-by-member.php
@@ -4,7 +4,7 @@ require_once("model-books-borrowed-by-member.php");
 
 $pageTitle = "Books Borrowed by Members";
 include "view-header.php";
-$books = selectBooksBorrowedbyMembers($_GET['id']);
+$books = selectBooksBorrowedbyMembers($_GET['iid']);
 include "view-books-borrowed-by-member.php";
 include "view-footer.php";
 ?>
