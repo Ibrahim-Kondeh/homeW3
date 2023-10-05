39a57f28f0ec250e973997d66bee16a26e88e97b
Ibrahim-Kondeh <122960252+Ibrahim-Kondeh@users.noreply.github.com>
2023-10-04 22:26:47 -0500

    Create view-books-borrowed-by-member.php

---
+++ view-books-borrowed-by-member.php
@@ -0,0 +1,32 @@
+<h1>Books Borrowed</h1> 
+<div class="table-responsive">
+  <table class="table">
+    <thead>
+      <tr>
+        <th>ID</th>
+         <th>Date Taken</th>
+          <th>Librarian</th>
+         <th>Return Date</th>
+    
+      </tr>
+     
+    </thead>
+    <tbody>
+    <?php
+     while ($borrowedbook = $borrowedbooks->fetch_assoc()) { 
+       ?>
+       <tr>
+       <td><?php echo $borrowedbook['book_id']; ?></td>
+       <td><?php echo $borrowedbook['date_borrowed']; ?></td>
+        <td><?php echo $borrowedbook['librarian']; ?></td>
+         <td><?php echo $borrowedbook['return_date']; ?></td>
+       </tr>
+       <?php 
+     }
+
+   ?> 
+
+      
+    </tbody>
+  </table>
+</div>
